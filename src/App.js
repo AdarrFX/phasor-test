3a39b8812ba71cebf2ab0aa1526d2baf8cb30369
Adarr <grenador@gmail.com>
2021-09-23 20:19:05 -0400

    Added API request using axios for foxxos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageBox from './ImageBox'
 
 function App() {
+
+  function displayBois () {
+    for (let i=0; i<10; i++){
+      <ImageBox />
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Does this change anything?
         </p>
+
+          {displayBois()}
+
+          <ImageBox />
+
         <a
           className="App-link"
           href="https://reactjs.org"
